dca01864a0ebba087f8f4e8f507a12eec1e741dd
Dermot Brennan <dermot.brennan@gmail.com>
2010-09-06 22:12:57 +0100

    Initial Commit

---
+++ src/com/google/gwt/sample/stockwatcher/client/NotLoggedInException.java
@@ -0,0 +1,17 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import java.io.Serializable;
+
+public class NotLoggedInException extends Exception implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  public NotLoggedInException() {
+    super();
+  }
+
+  public NotLoggedInException(String message) {
+    super(message);
+  }
+
+}
